22373f549f50b87ace3610b043c0c834439a7991
agarrigu <83713047+agarrigu@users.noreply.github.com>
2026-08-17 00:38:16 +0100

    scripts: createls: make more robust

--- scripts/createsls.sh
+++ scripts/createsls.sh
@@ -1,28 +1,53 @@
 #!/bin/bash
 
-declare -ar DH_FILES=(
-".bashrc"
-".gdbinit"
-".gitconfig"
-".tmux.conf"
-".vimrc"
-".zshenv"
-".zshrc"
-)
+fflag=
+while getopts f opt; do
+	case $opt in
+		f) fflag=1 ;;
+	esac
+done
+shift $((OPTIND - 1))
 
-declare -ar DH_DIRS=(
-".vim"
-".config/nvim"
-".config/foot"
-)
+DF_WD="$HOME/dotfiles"
+DH_TARGETS_DIR="$DF_WD/dollarhome"
+DC_TARGETS_DIR="$DF_WD/dotconfig"
+DH_LINKS_DIR="$HOME"
+DC_LINKS_DIR=${XDG_CONFIG_HOME:-"$HOME/.config"}
 
-for f in ${DH_FILES[@]}; do
-	[[ -f "$HOME/dotfiles/$f" ]] && [[ ! -f "$HOME/$f" ]] \
-	&& ln -s "$HOME/dotfiles/$f" "$HOME/$f"
+askforce=
+
+for f in $(ls "$DH_TARGETS_DIR") ; do
+	TARGET="$DH_TARGETS_DIR/$f"
+	LINK_NAME="$DH_LINKS_DIR/.$f" # NB! Dot in front of $f
+	if [[ -f "$LINK_NAME" ]] ; then
+		if [[ -z $fflag ]] ; then
+			echo "Error: Dotfile already exists: .$f" >&2
+			askforce=1
+			continue
+		else
+			rm -v "$LINK_NAME"
+		fi
+	fi
+	ln -sv "$TARGET" "$LINK_NAME"
 done
 
-for d in ${DH_DIRS[@]}; do
-	[[ -d "$HOME/dotfiles/$d" ]] && [[ ! -d "$HOME/$d" ]] \
-	&& ln -s "$HOME/dotfiles/$d" "$HOME/$d"
+[[ ! -d $DC_LINKS_DIR ]] && mkdir -v $DC_LINKS_DIR
+
+for d in $(ls "$DC_TARGETS_DIR") ; do
+	TARGET="$DC_TARGETS_DIR/$d"
+	LINK_NAME="$DC_LINKS_DIR/$d"
+	if [[ -d "$LINK_NAME" ]] ; then
+		if [[ -z $fflag ]] ; then
+			echo "Error: Directory already exists: $d" >&2
+			askforce=1
+			continue
+		else
+			# Not gonna use -f flag as there shouldn't be any
+			# protected files/directories
+			rm -rv "$LINK_NAME"
+		fi
+	fi
+	ln -sv "$TARGET" "$LINK_NAME"
 done
 
+[[ -n $askforce ]] && echo "Use -f flag to force overwrite" >&2
